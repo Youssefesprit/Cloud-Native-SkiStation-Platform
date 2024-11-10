4cf7f65041127f50ebf792a8c905875cb207acac
Chaima Saadallah <80220736+saadallahchaima@users.noreply.github.com>
2024-11-10 17:33:26 +0100

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
@@ -5,13 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
