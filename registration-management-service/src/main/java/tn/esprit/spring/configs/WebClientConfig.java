cf64a6c1509d095255071ec7dd88f7849e866f7f
Chaima Saadallah <80220736+saadallahchaima@users.noreply.github.com>
2024-11-10 09:53:21 +0100

    Update WebClientConfig.java

--- registration-management-service/src/main/java/tn/esprit/spring/configs/WebClientConfig.java
+++ registration-management-service/src/main/java/tn/esprit/spring/configs/WebClientConfig.java
@@ -5,11 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebClientConfig {
     @Bean
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
+    }
+
+
+
+     @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "http://192.168.192.131:4200")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Authorization", "Content-Type")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
