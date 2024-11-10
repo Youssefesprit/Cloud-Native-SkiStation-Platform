e3b275627cdd2c628625e9809d0a903bd83fe370
Chaima Saadallah <80220736+saadallahchaima@users.noreply.github.com>
2024-11-10 09:51:33 +0100

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 @SpringBootApplication
 @EnableConfigurationProperties
 @CrossOrigin(origins = "*")
@@ -15,5 +19,20 @@ public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://192.168.192.131:4200")); // Add your Angular app's origin
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
 }
 
