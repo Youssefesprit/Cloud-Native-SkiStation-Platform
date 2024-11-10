00be1bd5a9d779bfc7dcc1e1122113a7bcb4d13b
Chaima Saadallah <80220736+saadallahchaima@users.noreply.github.com>
2024-11-10 13:30:00 +0100

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
@@ -15,25 +15,10 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
-@CrossOrigin(origins = "*")
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
-    }
-
-    @Bean
-    public CorsWebFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://192.168.192.131:4200")); 
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        config.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
     }
 
 }
