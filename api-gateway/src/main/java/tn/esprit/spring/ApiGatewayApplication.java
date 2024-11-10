0b9a9cd2dfaf10756aea72de6f589166518e82a4
Chaima Saadallah <80220736+saadallahchaima@users.noreply.github.com>
2024-11-10 10:15:21 +0100

    Update ApiGatewayApplication.java

--- api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
+++ api-gateway/src/main/java/tn/esprit/spring/ApiGatewayApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties
